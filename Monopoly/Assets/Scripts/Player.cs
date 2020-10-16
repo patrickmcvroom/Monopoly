7843218d37da9260b18357a4be1a1de08adf1a46
Patrick McVroom <patrickmcvroom@gmail.com>
2020-10-16 17:31:22 +0100

    Created settings and player scripts

---
+++ Monopoly/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]int cash;
+    [SerializeField]bool isBankrupt;
+    [SerializeField]int position;
+
+
+    void Start()
+    {
+        cash = 1500;
+        isBankrupt = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cash = Mathf.Clamp(cash, 0, 50000);
+    }
+}
---
+++ Monopoly/Assets/Scripts/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField]int players = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        players = Mathf.Clamp(players, 0, 4);
+    }
+}
