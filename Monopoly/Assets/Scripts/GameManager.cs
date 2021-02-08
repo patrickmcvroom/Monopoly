fbb7b64b840708123b754301122da96a54aa9b11
Patrick McVroom <patrickmcvroom@gmail.com>
2021-02-08 11:09:33 +0000

    Added a type writer function to game manager class

--- Monopoly/Assets/Scripts/GameManager.cs
+++ Monopoly/Assets/Scripts/GameManager.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public enum GameState { START, PLAYER_ORDER, ROLL, MOVE, CELL_EVENT, END_TURN, GAMEOVER }
 
 public class GameManager : MonoBehaviour
 {
-    public GameState state;
+    public GameState State;
+    public TextMeshProUGUI DialogueText;
+    public string fullText;
 
     [SerializeField] private GameObject gameBoard;
     [SerializeField] private GameObject middleOfBoard;
@@ -14,16 +18,19 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Player playerOne;
     [SerializeField] private int playerTurn;
     [SerializeField] private new Camera camera;
+    [SerializeField] private float delay = 0.03f;
 
     private Board board;
-     
+    private string currentText = "";
+
+    // Called at initialization
     private void Start()
     {
-        state = GameState.START;
-        SetupScene();
+        State = GameState.START;
+        StartCoroutine(SetupScene());
     }
 
-    void SetupScene()
+    IEnumerator SetupScene()
     {
         // Set the camera position and rotation
         camera.transform.position = new Vector3(7.58f, 9.4f, 0f);
@@ -37,8 +44,25 @@ public class GameManager : MonoBehaviour
         board.Create(board.GetBoardWidth(), board.GetBoardWidth(), Vector3.zero);
         Instantiate(middleOfBoard, new Vector3(0f, 0.01f, 0f), Quaternion.identity);
 
-        // Generate the players
-        //playerOne.Create(board.GetCellPosition(15));
+        // Generate the player
+
+        // Setup the dialogue
+        fullText = "Welcome to Monopoly!";
+        StartCoroutine(TypeText());
+        yield return new WaitForSeconds(4f);
+        fullText = "Which player will go first?";
+        StartCoroutine(TypeText());
+        State = GameState.PLAYER_ORDER;
+    }
+
+    IEnumerator TypeText()
+    {
+        for (int i = 0; i < fullText.Length+1; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            DialogueText.text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
     }
 
     // Update is called once per frame
