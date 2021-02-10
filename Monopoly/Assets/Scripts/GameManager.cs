346ed5cd00493ced50e38044492248b034c7a7a5
Patrick McVroom <patrickmcvroom@gmail.com>
2021-02-10 22:54:47 +0000

    Gets dialogue text to respond to first roll!

--- Monopoly/Assets/Scripts/GameManager.cs
+++ Monopoly/Assets/Scripts/GameManager.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public enum GameState { START, PLAYER_ORDER, ROLL, MOVE, CELL_EVENT, END_TURN, GAMEOVER }
+public enum GameState { START, PLAYER_ONE_ROLL, PLAYER_TWO_ROLL, ROLL, MOVE, CELL_EVENT, END_TURN, GAMEOVER }
 
 public class GameManager : MonoBehaviour
 {
     public GameState state;
+    public GameObject playerOneHUD;
+    public GameObject playerTwoHUD;
     public GameObject dialoguePanel;
     public TextMeshProUGUI dialogueText;
     public TextMeshProUGUI playerOneNameText;
@@ -24,65 +26,22 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject rollButton;
     [SerializeField] private Player playerOne;
     [SerializeField] private Player playerTwo;
-    [SerializeField] private int playerTurn;
+    [SerializeField] private int playerTurn = 100;
     [SerializeField] private new Camera camera;
     [SerializeField] private float delay = 0.03f;
+    [SerializeField] private bool hasRolled = false;
 
     private Board board;
     private string currentText = "";
-    private int totalPlayers = 2;
+    private readonly int totalPlayers = 2;
+    private int playerOneFirstRoll;
+    private int playerTwoFirstRoll;
 
     // Called at initialization
     private void Start()
     {
         state = GameState.START;
         StartCoroutine(SetupScene());
-    }
-
-    private void Update()
-    {
-        switch(state)
-        {
-            case GameState.PLAYER_ORDER:
-                //Show dice and roll button
-                dice.gameObject.SetActive(true);
-                rollButton.SetActive(true);
-
-                // Set player turn
-                playerTurn = 1;
-                // Clamp the player turn variable
-                playerTurn %= totalPlayers;
-
-                break;
-            case GameState.ROLL:
-                //Show dice and roll button
-                dice.gameObject.SetActive(true);
-                rollButton.SetActive(true);
-
-                // If pl
-
-                break;
-            case GameState.MOVE:
-                //Hide dice and roll button
-                dice.gameObject.SetActive(false);
-                rollButton.SetActive(false);
-                //Hide the dialogue panel
-                dialoguePanel.SetActive(false);
-                break;
-            case GameState.CELL_EVENT:
-                //Show the dialogue panel
-                dialoguePanel.SetActive(true);
-                break;
-            case GameState.END_TURN:
-                //Hide the dialogue panel
-                dialoguePanel.SetActive(false);
-                break;
-            case GameState.GAMEOVER:
-                //Hide dice and roll button
-                dice.gameObject.SetActive(false);
-                rollButton.SetActive(false);
-                break;
-        }
     }
 
     IEnumerator SetupScene()
@@ -107,7 +66,9 @@ public class GameManager : MonoBehaviour
         playerOne.Create(new Vector3(board.GetCellPosition(0).x + 0.4f, board.GetCellPosition(0).y, board.GetCellPosition(0).z));
         playerTwo.Create(new Vector3(board.GetCellPosition(0).x - 0.4f, board.GetCellPosition(0).y, board.GetCellPosition(0).z));
 
-        // Set Player Units
+        // Set Player HUDs and Units
+        playerOneHUD.GetComponent<Image>().color = new Color32(254, 255, 196, 255);
+        playerTwoHUD.GetComponent<Image>().color = new Color32(254, 255, 196, 255);
         playerOne.cash = 1500;
         playerTwo.cash = 1500;
 
@@ -129,7 +90,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator TypeText()
     {
-        for (int i = 0; i < fullText.Length+1; i++)
+        for (int i = 0; i < fullText.Length + 1; i++)
         {
             currentText = fullText.Substring(0, i);
             dialogueText.text = currentText;
@@ -145,6 +106,75 @@ public class GameManager : MonoBehaviour
             dialogueText.text = currentText;
             yield return new WaitForSeconds(delay);
         }
-        state = GameState.PLAYER_ORDER;
+        playerTurn = 0;
+        state = GameState.PLAYER_ONE_ROLL;
+        dice.gameObject.SetActive(true);
+        rollButton.gameObject.SetActive(true);
+    }
+
+    public void NextTurn()
+    {
+        playerTurn += 1;
+        playerTurn %= totalPlayers;
+    }
+
+    IEnumerator ProcessPlayerOneRoll()
+    {
+        yield return new WaitForSeconds(3f);
+
+        GameObject currentPlayer = GameObject.FindGameObjectWithTag(playerTurn.ToString());
+        
+
+        fullText = currentPlayer.GetComponent<Player>().playerName + " has roled a " + diceNumber.ToString() + ".";
+        StartCoroutine(TypeText());
+        hasRolled = false;
+
+    }
+
+    IEnumerator ProcessPlayerTwoRoll()
+    {
+        yield return new WaitForSeconds(3f);
+        fullText = playerTwo.playerName + " has roled a " + diceNumber.ToString() + ".";
+        StartCoroutine(TypeText());
+        hasRolled = false;
+        NextTurn();
+    }
+
+    public void OnRoll()
+    {
+        hasRolled = true;
+
+        if(state == GameState.PLAYER_ONE_ROLL)
+        {
+            StartCoroutine(ProcessPlayerOneRoll());
+        }
+        else if(state == GameState.ROLL)
+        {
+            GameObject.FindGameObjectWithTag(playerTurn.ToString());
+        }
+    }
+
+    // ----- UPDATE FUNCTION -----
+    private void Update()
+    {
+        if(playerTurn == 0)
+        {
+            // Enlarge Player 1 HUD, and change its colour
+            playerOneHUD.gameObject.transform.localScale = new Vector3(1.14f, 1.14f, 1.14f);
+            playerOneHUD.GetComponent<Image>().color = new Color32(253, 100, 100, 255);
+            // Reduce Player 2 HUD
+            playerTwoHUD.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            playerTwoHUD.GetComponent<Image>().color = new Color32(254, 255, 196, 255);
+        }
+
+        if(playerTurn == 1)
+        {
+            // Enlarge Player 2 HUD and change its colour
+            playerTwoHUD.gameObject.transform.localScale = new Vector3(1.14f, 1.14f, 1.14f);
+            playerTwoHUD.GetComponent<Image>().color = new Color32(253, 100, 100, 255);
+            // Reduce Player 1 HUD
+            playerOneHUD.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            playerOneHUD.GetComponent<Image>().color = new Color32(254, 255, 196, 255);
+        }
     }
 }
